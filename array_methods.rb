e457f24454af3e9adf394a65b0431a501937f0dc
lorelei522 <lorelei522@gmail.com>
2016-10-30 14:31:26 -0400

    Create methods and pass test for first_duplicate and symbolize

--- array_methods.rb
+++ array_methods.rb
@@ -9,5 +9,27 @@ def average(array)
 	(array.reduce(:+)).to_f / (array.length).to_f
 end
 
-array=[1,2,3,4]
-p average(array)	
+# array=[1,2,3,4]
+# p average(array)	
+
+
+# def super_compact(array)
+# 	 array.reject {|element| element.empty?}
+# end	
+
+# array = [:bob, "", nil, [], "joe"]
+# p super_compact(array)
+
+#come back to this
+def tight_zip(array1,array2)
+
+end	
+
+#if i wanted to find all i would just change to array.find_all
+def first_duplicate(array)
+	array.find {|element| array.count(element) > 1}
+end	
+
+def symbolize(array)
+	array.map {|element| element.to_sym}
+end	
